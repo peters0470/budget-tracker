b18c9e1f28c743c95b728d13873a489533dda0f4
unknown <peters0470@gmail.com>
2021-03-05 14:35:47 -0600

    started service worker file

---
+++ public/service-worker.js
@@ -0,0 +1,18 @@
+const FILES_TO_CACHE = [
+    "./index.html",
+    "./css/style.css",
+    "./js/index.js",
+    "./js/idb.js",
+    "./manifest.webmanifest",
+    "./icons/icon-72x72.png",
+    "./icons/icon-96x96.png",
+    "./icons/icon-128x128.png",
+    "./icons/icon-144x144.png",
+    "./icons/icon-152x152.png",
+    "./icons/icon-192x192.png",
+    "./icons/icon-384x384.png",
+    "./icons/icon-512x512.png"
+  ];
+
+const CACHE_NAME = "dude-cache-v1";
+const DATA_CACHE_NAME = "love-cache-v1";
